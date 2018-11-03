e7dca89b49cf3e4c6c5d1a603ec25668675553b1
lintangwisesa <lintangwisesa@ymail.com>
2018-11-03 12:42:50 +0700

    basic python

---
+++ 20.for_loop.py
@@ -0,0 +1,17 @@
+
+for huruf in 'Lintang':
+    print(huruf)
+
+for index in range(6):
+    print(index)
+
+for index in range(6, 10):
+    print(index)
+
+kawan = ['Andi', 'Budi', 'Caca']
+
+for nama in kawan:
+    print(nama)
+
+for i in range(len(kawan)):
+    print(kawan[i])
