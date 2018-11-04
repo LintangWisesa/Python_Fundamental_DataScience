df89471b303774967dcf3c5b8e3e3252d69621a9
lintangwisesa <lintangwisesa@ymail.com>
2018-11-04 11:29:45 +0700

    basic python

--- 43.numpy_trigonometri.py
+++ 43.numpy_trigonometri.py
@@ -1,10 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-x = np.arange(0, 3*np.pi, 0.1)
+x = np.arange(0, 3 * np.pi, 0.1)
 # y = np.sin(x)
 # y = np.cos(x)
 y = np.tan(x)
+z = np.cos(0)
 
-plt.plot(x,y)
-plt.show()
+print(y)
+print(z)
