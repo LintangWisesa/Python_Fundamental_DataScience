f54c9bb2822e6ade463595a97ce5236e6ae023c0
lintangwisesa <lintangwisesa@ymail.com>
2018-11-26 10:52:51 +0700

    python data science

--- 3 Pandas/2_viewdata.py
+++ 3 Pandas/2_viewdata.py
@@ -21,4 +21,21 @@ df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
 # print(df.sort_values(by='B'))
 # print(df.sort_values(by='B', ascending=False))
 
+# print(df['A'])
+# print(df[0:3])
 
+# print(df.loc[dates[0]])
+# print(df.loc[:,['A','B']])
+# print(df.loc['20130102':'20130104',['A','B']])
+
+# print(df.loc[dates[0],'A'])
+# print(df.at[dates[0],'A'])
+
+# print(df.iloc[3])
+# print(df.iloc[3:5,0:2])
+# print(df.iloc[[1,2,4],[0,2]])
+# print(df.iloc[1:3,:])
+
+# print(df.iloc[:,1:3])
+# print(df.iloc[1,1])
+# print(df.iat[1,1])
