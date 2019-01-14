9e037715386fd46887d826b6e281d101587495e8
lintangwisesa <lintangwisesa@ymail.com>
2019-01-14 15:29:26 +0700

    python ds

---
+++ 0 Python Fundamental/4.z_find_string.py
@@ -0,0 +1,16 @@
+# Cari Huruf
+
+kalimat = 'Hari ini Andi tidak sekolah.'
+cari = 'a'
+kalmina = kalimat.upper().replace(cari.upper(), '')
+jumlah = len(kalimat) - len(kalmina)
+print(jumlah)
+
+################################
+
+# Cari Kata
+
+kalimat = 'sekolah Hari ini Andi tidak Sekolah.'
+cari = 'sekolah'
+kalcari = kalimat.upper().replace(cari.upper(), '')
+print(int((len(kalimat) - len(kalcari)) / len(cari)))
