0204ae390aafde2a1f75e333018554a03754fb4d
lintangwisesa <lintangwisesa@ymail.com>
2019-01-15 13:42:45 +0700

    python datascience

--- 0 Python Fundamental/6.math.py
+++ 0 Python Fundamental/6.math.py
@@ -1,5 +1,11 @@
 from math import *
+print(pi)
+print(floor(3.9))
+print(ceil(3.2))
 
-print(floor(3.7))
-print(ceil(3.7))
-print(sqrt(36))
+# atau bisa juga ditulis sbb:
+
+import math
+print(math.pi)
+print(math.floor(3.9))
+print(math.ceil(3.2))
