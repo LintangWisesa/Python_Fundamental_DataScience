08d4eb0ba723af44ab3b3d49a97823b3d4f03ed0
lintangwisesa <lintangwisesa@ymail.com>
2019-01-17 13:55:49 +0700

    python datascience

---
+++ 0 Python Fundamental/15.zkurs_uang.py
@@ -0,0 +1,17 @@
+
+# konverter Rp IDR <=> $ USD
+# nilai = nilai uang
+# konversi = 'USD-IDR' / 'IDR-USD' 
+
+def konversi(nilai, konversi):
+    if konversi == 'USD-IDR':
+        kurs = 14000
+        print('USD', nilai, 'setara = IDR', nilai * kurs)
+    elif konversi == 'IDR-USD':
+        kurs = 0.00007
+        print('IDR', nilai, 'setara = USD', nilai * kurs)
+    else:
+        print('Mohon maaf, hanya bisa konversi USD <=> IDR')
+
+konversi(2, 'USD-IDR')
+konversi(14000, 'IDR-USD')
---
+++ 0 Python Fundamental/15.zluas_trapesium.py
@@ -0,0 +1,5 @@
+
+def luasTrapesium(alas, atap, tinggi):
+    print((alas + atap) * (tinggi/2))
+
+luasTrapesium(8, 10, 4)
