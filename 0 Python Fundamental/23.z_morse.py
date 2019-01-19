fe9687957df7e5d43cce16649151bc75fe80764e
lintangwisesa <lintangwisesa@ymail.com>
2019-01-19 14:56:14 +0700

    python datascience

--- 0 Python Fundamental/23.z_morse.py
+++ 0 Python Fundamental/23.z_morse.py
@@ -25,7 +25,17 @@ morse = {
     'w': '.__',
     'x': '_.._',
     'y': '_.__',
-    'z': '__..'
+    'z': '__..',
+    '1': '.____',
+    '2': '..___',
+    '3': '...__',
+    '4': '...._',
+    '5': '.....',
+    '6': '_....',
+    '7': '__...',
+    '8': '___..',
+    '9': '____.',
+    '0': '_____'
 }
 
 def ubahkemorse(teks):
@@ -35,4 +45,12 @@ def ubahkemorse(teks):
         hasil += morse[i] + ' / '
     print(hasil)
 
+def ubahdarimorse(teks):
+    kalimat = teks.split(' / ')
+    hasil = ''
+    for i in kalimat:
+        hasil += list(morse.keys())[list(morse.values()).index(i)] 
+    print(hasil)
+    
 ubahkemorse('Lintang Wisesa')
+ubahdarimorse('._.. / .. / _. / _ / ._ / _. / __.')
