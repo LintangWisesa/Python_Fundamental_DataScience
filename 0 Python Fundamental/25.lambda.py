c9da1d38c513c6a64bf07d371dfc060812ad0a34
lintangwisesa <lintangwisesa@ymail.com>
2019-01-21 16:57:35 +0700

    python datascience

---
+++ 0 Python Fundamental/25.lambda.py
@@ -0,0 +1,19 @@
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+
+#######################################
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
