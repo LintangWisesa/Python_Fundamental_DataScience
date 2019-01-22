ed7e305bfee13514c4da36774a3f74109ff1c1bc
lintangwisesa <lintangwisesa@ymail.com>
2019-01-22 14:38:28 +0700

    python datascience

---
+++ 0 Python Fundamental/24.date_indonesia.py
@@ -0,0 +1,30 @@
+
+import datetime
+x = datetime.datetime.now()
+
+hari = {
+    'Monday': 'Senin',
+    'Tuesday': 'Selasa',
+    'Wednesday': 'Rabu',
+    'Thursday': 'Kamis',
+    'Friday': 'Jum\'at',
+    'Saturday': 'Sabtu'
+}
+
+bulan = {
+    'January': 'Januari',
+    'February': 'Februari',
+    'March': 'Maret',
+    'April': 'April',
+    'May': 'Mei',
+    'June': 'Juni',
+    'July': 'Juli',
+    'August': 'Agustus',
+    'September': 'September',
+    'October': 'Oktober',
+    'November': 'November',
+    'December': 'Desember'
+}
+
+print(hari[x.strftime('%A')])
+print(bulan[x.strftime('%B')])
