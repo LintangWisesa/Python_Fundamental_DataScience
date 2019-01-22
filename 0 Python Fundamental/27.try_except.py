1d806617fc95c85f850d29e5c36500eb38c61bd8
lintangwisesa <lintangwisesa@ymail.com>
2019-01-22 15:52:20 +0700

    python datascience

--- 0 Python Fundamental/27.try_except.py
+++ 0 Python Fundamental/27.try_except.py
@@ -7,6 +7,8 @@
 
 # run & coba inputkan bukan angka
 
+#####################################
+
 try:
     nilai = 10/0
     angka = int(input('Ketik angka: '))
@@ -15,3 +17,15 @@ except ZeroDivisionError:
     print('Tak bisa dibagi 0!')
 except ValueError:
     print('Input bukan angka!')
+
+#####################################
+
+
+# try:
+#     nilai1 = int(input('Ketik angka 1 : '))
+#     nilai2 = int(input('Ketik angka 2 : '))
+#     print(nilai1 / nilai2)
+# except ValueError:
+#     print('Maaf, hanya menerima input berupa angka')
+# except ZeroDivisionError:
+#     print('Jangan dibagi 0 dong!')
