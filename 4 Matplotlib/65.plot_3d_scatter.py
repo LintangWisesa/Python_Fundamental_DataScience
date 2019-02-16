dad900e9fbecb442232c7edca9ed0bc1f5c619fc
lintangwisesa <lintangwisesa@ymail.com>
2019-02-16 17:49:38 +0700

    python datascience

---
+++ 4 Matplotlib/65.plot_3d_scatter.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+x = [0,1,2,3,4,5,6,7,8,9]
+y = [0,1,2,3,4,5,6,7,8,9]
+z = [[0,1,2,3,4,5,6,7,8,9]]
+
+ax.scatter(np.array(x), np.array(y), np.array(z), color='red', marker='*')
+ax.set_xlabel('Nilai x')
+ax.set_ylabel('Nilai y')
+ax.set_zlabel('Nilai z')
+
+plt.show()
---
+++ 4 Matplotlib/66.plot_3d_bar.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+x = [0,1,2,3,4,5,6,7,8,9]
+y = [0,1,2,3,4,5,6,7,8,9]
+z = np.zeros(10)
+
+dx = np.ones(10)
+dy = np.ones(10)
+dz = [0,1,2,3,4,5,6,7,8,9]
+
+ax.bar3d(
+    np.array(x), 
+    np.array(y), 
+    np.array(z),
+    np.array(dx),
+    np.array(dy),
+    np.array(dz),
+)
+
+ax.set_xlabel('Nilai x')
+ax.set_ylabel('Nilai y')
+ax.set_zlabel('Nilai z')
+
+plt.show()
