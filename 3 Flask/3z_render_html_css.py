30ff986c6f2ef4ef8ef68b92d132b4edbeefd430
lintangwisesa <lintangwisesa@ymail.com>
2019-04-23 12:30:51 +0700

    python datascience

--- 3 Flask/3z_render_html_css.py
+++ 3 Flask/3z_render_html_css.py
@@ -11,6 +11,7 @@ def index():
 def about():
     return render_template('aboutVarCss.html')
     # render html file from templates dir
+    # html with css file on 'static' dir! dir name must be 'static'!
 
 if __name__ == '__main__':
     app.run(debug = True)
