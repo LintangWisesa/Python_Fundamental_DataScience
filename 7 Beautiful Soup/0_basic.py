6d34ff1d875167edb97bb1a9a411ce68472b4e7d
lintangwisesa <lintangwisesa@ymail.com>
2019-05-14 11:44:31 +0700

    python datascience

---
+++ 7 Beautiful Soup/0_basic.py
@@ -0,0 +1,16 @@
+### web scraping: pip install beautifulsoup4
+from bs4 import BeautifulSoup
+
+### from html text:
+soup = BeautifulSoup('<p>Some<b>bad<i>HTML', 'html.parser')
+print(soup)
+
+### from html file:
+soup = BeautifulSoup(open('0.html', 'r'), 'html.parser')
+print(soup)
+
+### from website:
+import requests
+r = requests.get("https://ezalin.com")
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup)
