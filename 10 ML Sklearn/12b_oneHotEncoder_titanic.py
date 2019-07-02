f43741521c9c1302507d955ab01777a5db02fd83
lintangwisesa <lintangwisesa@ymail.com>
2019-07-02 21:09:32 +0700

    compose

--- 10 ML Sklearn/12b_oneHotEncoder_titanic.py
+++ 10 ML Sklearn/12b_oneHotEncoder_titanic.py
@@ -4,76 +4,75 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 data = sb.load_dataset('titanic')
+# print(data.columns.values)
+
+# 1 labelling
+from sklearn.preprocessing import LabelEncoder
+label = LabelEncoder()
+
+data['sex'] = label.fit_transform(data['sex'])
+# print(label.classes_)       # ['female' 'male']
+
+data['who'] = label.fit_transform(data['who'])
+# print(label.classes_)       # 'child' 'man' 'woman' 0  1  2
+
+data['adult_male'] = label.fit_transform(data['adult_male'])
+# print(label.classes_)      # [False  True]  
+
+data['alone'] = label.fit_transform(data['alone'])
+# print(label.classes_)       # [False  True]
+
+data = data.drop(
+    ['embarked', 'class', 'deck', 'embark_town', 'alive'],
+    axis=1
+)
+data = data.dropna(subset = ['age'])
 # print(data)
 
-# ==========================================
-# label encoder
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-dfle = data
-dfle['sex'] = le.fit_transform(dfle['sex'])
-# print(list(le.classes_))        # ['female', 'male']
-dfle['who'] = le.fit_transform(dfle['who'])
-# print(list(le.classes_))      # ['child', 'man', 'woman']
-dfle['adult_male'] = le.fit_transform(dfle['adult_male'])
-# print(list(le.classes_))        # [False, True]
-dfle['alone'] = le.fit_transform(dfle['alone'])
-# print(list(le.classes_))        # [False, True]
-
-# ==========================================
-# clean missing data value
-# print(dfle[['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'who', 'adult_male', 'alone']].isnull())
-dfle = dfle.dropna(subset = ['age'])
-# print(dfle)
-
-# ==========================================
-# define x feature & y target
-x = dfle[['sex', 'who', 'adult_male', 'alone', 'pclass', 'age', 'sibsp', 'parch', 'fare']]
+# Split: feature X & target Y
+x = data.drop(['survived'], axis=1)
 # print(x.iloc[0])
-y = dfle['survived']
+y = data['survived']
 # print(y)
 
-# ==========================================
-# one hot encoder
+# ONE HOT ENCODER
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-ct = ColumnTransformer(
-    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0, 1, 2, 3])],
+coltrans = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [1, 6])],    # 1,6 = index col yang akan di-one hot encoding
     remainder='passthrough'
 )
-
-x = np.array(ct.fit_transform(x), dtype=np.float64)
+x = np.array(coltrans.fit_transform(x), dtype=np.float64)
 # print(x[0])
-# [    0.    1.    0.    1.      0.         0.            1.             1.          0.         3.    22.    1.      0.    7.25]
-# [ female male child   man   woman  adultman_false  adultman_true  alone_false  alone_true  pclass  age   sibsp  parch    fare]
+# [ 0.    1.    0.    1.    0.    3.     22.    1.    0.    7.25     1.    0.  ]
+# fm   male  child  man  woman  pclass  age  sibsp  par   fare   adultM   alone
 
-# ==========================================
-# split datasets
+# splitting
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(
-    x, 
-    y, 
-    test_size=.1
+xtrain, xtest, ytrain, ytest = train_test_split(
+    x,
+    y,
+    test_size = .1
 )
-# print(x_train[0])
-# print(y_train.iloc[0])
-# print(x_test[0])
-# print(y_test.iloc[0])
+# print(xtrain[0])
+# print(ytrain.iloc[0])
+# print(xtest[0])
+# print(ytest.iloc[0])
 
-# ============================================
 # logistic regression
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression(solver='liblinear')
-model.fit(x_train, y_train)
-print(round(model.score(x_test, y_test) * 100, 2), '%')
+model.fit(xtrain, ytrain)
 
-# ============================================
-# predict
-# [female male] [child man woman] [adultman_false adultman_true] [alone_false alone_true] pclass age sibsp parch fare]
-print(model.predict([[0., 1., 0., 1., 0., 0., 1., 0., 1., 3., 32., 0., 0., 7.8542]]))
+# print(round(model.score(xtest, ytest) * 100, 2), '%')
+# print(xtest[0])
+# print(ytest.iloc[0])
+# print(model.predict(xtest[0].reshape(1, -1)))
 
-# predict x_test
-print(x_test[0])
-print(model.predict(x_test[0].reshape(1, -1)))
-print(y_test.iloc[0])
+# [ 0.    1.    0.    1.    0.    3.     22.    1.    0.    7.25     1.    0.  ]
+# fm   male  child  man  woman  pclass  age  sibsp  par   fare   adultM   alone
+# print(model.predict([[0, 1, 1, 0, 0, 1, 5, 1, 2, 100, 0, 0]]))
+
+import joblib
+joblib.dump(model, 'modelTitanic')
