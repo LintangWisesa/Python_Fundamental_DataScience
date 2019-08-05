03ebbea012ad9ce7fa478d351aeb4279bec5a555
lintangwisesa <lintangwisesa@ymail.com>
2019-08-05 09:14:19 +0700

    python datascience

---
+++ 0 Python Fundamental/32j_json_to_csv.py
@@ -0,0 +1,18 @@
+# ================================
+# read json
+import json
+
+with open('32k_data.json', 'r') as f:
+    data = json.load(f)
+
+print(data)
+
+# ================================
+# import to csv
+import csv
+
+with open('32k_out.csv', 'w', newline = '') as fileku:
+    kolom = ['nama', 'usia', 'kota']
+    tulis = csv.DictWriter(fileku, fieldnames = kolom)
+    tulis.writeheader()
+    tulis.writerows(data)
