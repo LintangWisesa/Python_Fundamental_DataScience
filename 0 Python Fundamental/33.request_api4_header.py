295b91f967c1c806bc4a82b140fb6dc06933186a
lintangwisesa <lintangwisesa@ymail.com>
2019-08-06 12:08:03 +0700

    python datascience

--- 0 Python Fundamental/33.request_api4_header.py
+++ 0 Python Fundamental/33.request_api4_header.py
@@ -1,14 +1,19 @@
-
 import requests
 
-# cari kebab di jakarta via zomato api
-url = 'https://developers.zomato.com/api/v2.1/search?entity_id=74&entity_type=city&q=kebab'
+host = 'https://developers.zomato.com/api/v2.1'
+cari = input('Kamu mau makan apa? : ')
+url = f'{host}/search?entity_id=74&q={cari}'
 apikey = 'd1b8cea4661437f143641aabd01544f7'
 headers = {
-    'user-key': apikey
+    'user-key': apikey,
+    'Content-Type': 'application/json'
 }
 
 data = requests.get(url, headers=headers)
-daftarMakanan = data.json()
-print(daftarMakanan)
-
+listResto = data.json()['restaurants']
+for i in listResto:
+    print(
+        '⭐ ', i['restaurant']['name'], ':', 
+        i['restaurant']['location']['address']
+    )
+# print(len(listResto))
