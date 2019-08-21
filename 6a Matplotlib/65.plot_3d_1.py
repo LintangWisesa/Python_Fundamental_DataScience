c1cf917d4941dd071ec3cb8bdffefada2cf262eb
lintangwisesa <lintangwisesa@ymail.com>
2019-08-21 15:21:11 +0700

    python datascience

--- 6a Matplotlib/65.plot_3d_1.py
+++ 6a Matplotlib/65.plot_3d_1.py
@@ -1,3 +1,5 @@
+# to zoom in/out: right click + drag mouse
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 import numpy as np
--- 6a Matplotlib/66.plot_3d_barReadCsv.py
+++ 6a Matplotlib/66.plot_3d_barReadCsv.py
@@ -17,6 +17,10 @@ with open('54_datarokok.csv', 'r') as fileku:
         dz1.append(float(x[1]))
         dz2.append(float(x[2]))
 
+print(prov)
+print(dz1)
+print(dz2)
+
 prov.remove(prov[0])
 dz1.remove(dz1[0])
 dz2.remove(dz2[0])
@@ -26,6 +30,11 @@ y1 = np.zeros(len(dz1))
 z1 = np.zeros(len(dz1))
 dx1 = np.repeat(.1, len(dz1))
 dy1 = np.repeat(.25, len(dz1))
+# print(x1)
+# print(y1)
+# print(z1)
+# print(dx1)
+# print(dy1)
 
 x2 = np.arange(len(dz2))
 y2 = np.repeat(.6, len(dz2))
@@ -56,10 +65,11 @@ ax.bar3d(
 ax.set_xlim(0, len(dz1))
 ax.set_ylim(0, 1)
 
-ax.set_xlabel('Nilai x')
-ax.set_ylabel('Nilai y')
-ax.set_zlabel('Nilai z')
+# ax.set_xlabel('Nilai x')
+# ax.set_ylabel('Nilai y')
+# ax.set_zlabel('Nilai z')
 
+plt.yticks([.14, .74], [2015, 2016])
 plt.xticks(x1, prov)
 plt.xticks(rotation = 90)
 plt.show()
