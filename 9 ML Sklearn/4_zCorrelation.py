f12ef61d75fee39b3666e9783228033c99d4ef29
lintangwisesa <lintangwisesa@ymail.com>
2019-09-03 12:19:51 +0700

    python datascience

---
+++ 9 ML Sklearn/4_zCorrelation.py
@@ -0,0 +1,32 @@
+# mencari kolom yg menentukan target
+# cari correlation antar kolom di dataframe
+
+from sklearn import linear_model
+import pandas as pd
+import numpy as np
+
+data = {
+    'luas': [2600, 3000, 3200, 3600, 4000],
+    'kamar': [3, 4, 1, 3, 5],
+    'usia': [20, 15, 18, 30, 8],
+    'harga': [550000, 565000, 610000, 595000, 760000]
+}
+df = pd.DataFrame(data)
+print(df)
+
+# ==================================
+# find correlation & plot on a heatmap
+
+corr = df.corr()
+print(corr)
+
+# plot with seaborn heatmap
+import seaborn as sb
+sb.heatmap(corr)
+plt.show()
+
+# plot with matplotlib heatmap
+import matplotlib.pyplot as plt
+plt.imshow(corr, cmap='hot_r')
+plt.colorbar()
+plt.show()
