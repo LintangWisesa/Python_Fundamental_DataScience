38ed954e5afbc185a4cda3f29230f851f2dc92cd
lintangwisesa <lintangwisesa@ymail.com>
2020-02-27 14:21:13 +0700

    numpy

--- 4 NumPy/31.numpy_array.py
+++ 4 NumPy/31.numpy_array.py
@@ -1,7 +1,30 @@
 
 # install numpy: $ pip install numpy
-
 import numpy as np
+
+# =======================================
+# Why we use NUMPY?
+
+# show only genap [2,4,6,8,12,10] 
+# no numpy
+x = [2,4,1,6,8,3,12,10]
+y = list(filter(lambda i: i%2==0, x))
+print(y)
+# using numpy
+z = np.array(x)
+print(z[z % 2 == 0])
+
+# show [2,4,6]
+# no numpy
+x = [(1, 2), (3, 4), (5, 6)]
+y = list(map(lambda i: i[1], x))
+print(y)
+# using numpy
+z = np.array(x)
+y = z[:,1]
+print(list(y))
+
+# =======================================
 
 z = np.array([1,2,3,4,5,6,7,8,9,10])
 
@@ -10,6 +33,9 @@ print(z[0])
 print(z[-1])
 print(z[3::2])
 print(z[z % 2 != 0])
+
+# y = list(filter(lambda i: i%2==0, x))
+# print(y)
 
 # z[z % 2 != 0] = -1
 # print(z)
@@ -44,11 +70,23 @@ print(a[0:,3])          # [0:,3] means all rows, index 3
 print(a[0:2,3])         # [0:2,3] from row 0-2 (2 is not included), index 3
 print(a[0:3, [0,2]])    # row 0-2, index 0 & 2
 print(a[0:, [0, -1]])   
+print(a[:, [3,2,1,0]])
+print(a[:, [3,2,1,0,0,1,2,3]])
 
 print(a[0:3:2])         # start : stop : step
 print(a[1::2])
-print(a[1::2], 1)
+print(a[1::2, 1])
 
 #  3 dimensi
-b = np.array([[[1, 2, 3]]])
-print(b[:,:,2])
+a = [
+    [[1, 2], [3, 4], [5, 6]],
+    [[7, 8], [9, 10], [11, 12]],
+    [[13, 14], [15, 16], [17, 18]]
+]
+a = np.array(a)
+
+print(a[1][2][0])
+print(a[1,2,0])
+print(a[:, :, 1])
+print(a[:, :, [1, 0]])
+print(a[:, :, [1, 0, 0, 1]])
---
+++ 4 NumPy/32a.numpySifat.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# =====================================================
+# 1. numpy arrat dpt dibuat dr set, 
+#    tp tetap sifat set = no index
+
+a = np.array({1,2,3,4,5})
+print(a)
+# print(a[0]) # error!
+
+# =====================================================
+# 2. tidak boleh sisipkan str di beda numpy array int
+
+a = np.array([1,2,3,4,5])
+# a[0] = 'Andi' 
+# print(a)  # akan error krn tdk bisa sisipkan str ke numpy int
+a[0] = 12 
+print(a)  # bisa krn sisipkan int ke numpy int
+
+# =====================================================
+# 3. int yg disisipkan ke numpy str akan berubah jd str,
+#    dg panjang digit tidak lebih dr pjg karakter elemen terpjg
+
+a = np.array(['a', 'b', 'c'])
+a[0] = 120
+print(a)  # ['1' 'b' 'c']
+
+a = np.array(['andi', 'budi', 'caca'])
+a[0] = 120
+print(a)  # ['120' 'budi' 'caca']
